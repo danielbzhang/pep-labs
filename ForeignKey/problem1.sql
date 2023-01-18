c41d33e486cc7cec4716abb73c4f31d0d7fc4952
Daniel B. Zhang <daniel6628@gmail.com>
2023-01-18 01:50:56 +0000

    Saved progress on InnerJoin 2023-01-18 01:50:56 +0000

---
+++ ForeignKey/problem1.sql
@@ -0,0 +1,7 @@
+-- CREATE TABLE post(id SERIAL PRIMARY KEY, post VARCHAR(255), user_fk int REFERENCES user(id));
+
+CREATE TABLE post (
+id SERIAL PRIMARY KEY,
+post varchar(255),
+user_fk int REFERENCES user(id)
+);
