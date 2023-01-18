113f7b4eb918b6e2a76a667af236622f27220d27
Daniel B. Zhang <daniel6628@gmail.com>
2023-01-18 16:29:57 +0000

    Saved progress on InnerJoin 2023-01-18 16:29:57 +0000

--- InnerJoin/problem1.sql
+++ InnerJoin/problem1.sql
@@ -1,6 +1,2 @@
-SELECT student.id, student.student_name 
-FROM student 
-INNER JOIN class
-ON student.class_title = class.class_title
--- WHERE class.teacher_name = "Ms. Lovelace";
-WHERE class.class_title = "Physics" OR class.class_title = "Math";
+SELECT student.id, student.student_name FROM student INNER JOIN class ON student.class_title = class.class_title WHERE class.teacher_name = 'Ms. Lovelace';
+-- WHERE class.class_title = "Physics" OR class.class_title = "Math";
